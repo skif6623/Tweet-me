b4ad5a292b6289c548fa5c666b1cf2bd93b5d08f
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-05-06 23:20:07 +0300

    styled nav buttons

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 import {useLocation} from "react-router-dom";
-import {Bg} from "./HomePage.styled";
+import {Bg, NavBg} from "./HomePage.styled";
 
 export const HomePage = () => {
 	const location = useLocation();
 
 	return (
 		<Bg>
-			<NavLink to="/">Home</NavLink>
-			<NavLink to="/tweets" state={{from: location}}>
-				Tweets
-			</NavLink>
-			<div>Welcome to Tweet me</div>
+			<NavBg>
+				<NavLink to="/">Home</NavLink>
+				<NavLink to="/tweets" state={{from: location}}>
+					Tweets
+				</NavLink>
+				<p>Welcome to Tweet me</p>
+			</NavBg>
 		</Bg>
 	);
 };
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -2,8 +2,19 @@ import styled from "styled-components";
 import bg_image from "../../images/social-media-3846597.webp";
 
 export const Bg = styled.main`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
 	width: 100vw;
 	height: 100vh;
 
 	background-image: url(${bg_image});
+`;
+
+export const NavBg = styled.div`
+	width: 400px;
+	height: 300px;
+
+	background-color: red;
 `;
