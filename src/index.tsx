1f0c98538e3954925423bbdc81971e98df052f7d
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-06-04 23:42:16 +0300

    add ts template

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import {store} from "./redux/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
