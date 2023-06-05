173af665413a339d0a2df6ad9b834a8f603c733f
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-06-06 00:35:12 +0300

    rewrite userSlice on typescript

---
+++ src/redux/userSlice.ts
@@ -0,0 +1,41 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+import {addStatus, getUsers, moreUsers, removeStatus} from "./operations";
+
+type UserType = {
+	avatar: string;
+	follow: boolean;
+	followers: number;
+	id: string;
+	tweets: number;
+	user: string;
+};
+
+type InitialStateType = UserType[];
+
+const initialState: InitialStateType = [];
+
+export const userSlice = createSlice({
+	name: "users",
+	initialState,
+	reducers: {},
+	extraReducers: builder => {
+		builder
+			.addCase(getUsers.fulfilled, (_, action: PayloadAction<UserType[]>) => {
+				return action.payload;
+			})
+			.addCase(moreUsers.fulfilled, (state, action: PayloadAction<UserType[]>) => {
+				return [...state, ...action.payload];
+			})
+			.addCase(addStatus.fulfilled, (state, action: PayloadAction<UserType>) => {
+				const idx = state.findIndex(user => user.id === action.payload.id);
+				state.splice(idx, 1, action.payload);
+			})
+			.addCase(removeStatus.fulfilled, (state, action: PayloadAction<UserType>) => {
+				const idx = state.findIndex(user => user.id === action.payload.id);
+				state.splice(idx, 1, action.payload);
+			});
+	},
+});
+
+export const userReducer = userSlice.reducer;
