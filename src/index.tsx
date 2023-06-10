5e1f2123a7bf2253bb2eeabb60799469d0ed3018
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2023-06-10 13:46:24 +0300

    fix refresh bug on gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<BrowserRouter basename="go-it-test-task">
+			<BrowserRouter basename="/go-it-test-task">
 				<App />
 			</BrowserRouter>
 		</Provider>
